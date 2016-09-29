1589c0c3f2cb281e2ceff359f3fe5afe02b4a03c
Benjamin Congdon <bencongdon96@gmail.com>
2016-09-29 15:46:58 -0500

    Twitter tracker documentation

--- bolero/trackers/twitter.py
+++ bolero/trackers/twitter.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 class Tweet(db.Model):
+    """ Model to hold a single tweet """
     id = db.Column(db.BigInteger, primary_key=True)
     text = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
@@ -17,6 +18,7 @@ class Tweet(db.Model):
 
 
 class FollowerCount(db.Model):
+    """ Model to hold a single measurement of the user's follower count """
     __tablename__ = "twitterfollowercount"
     date = db.Column(db.DateTime(timezone=True), default=datetime.now,
                      primary_key=True)
@@ -26,6 +28,7 @@ class FollowerCount(db.Model):
 @requires('twitter.consumer_key', 'twitter.consumer_secret',
           'twitter.access_token_key', 'twitter.access_token_secret')
 def handle_authentication(config):
+    """ Authenticate and return an authenticated tweepy API object """
     auth = tweepy.OAuthHandler(config['twitter.consumer_key'],
                                config['twitter.consumer_secret'])
     auth.set_access_token(config['twitter.access_token_key'],
@@ -48,24 +51,33 @@ def save_tweet(t):
 @scheduler.scheduled_job('interval', hours=1)
 def get_tweets():
     """
-    Saves all tweets sent by authenticated user until it finds tweets that have
-    already been saved.
+    Grabs and saves all tweets sent by authenticated user until it finds
+    tweets that have already been saved.
     """
     api = handle_authentication()
     page = 1
     while True:
         logger.info("Scraping page {0} of tweets.".format(page))
         tweet_page = api.user_timeline(count=100, page=page)
+
+        # Get list of tweet_ids
         tweet_ids = map(lambda t: t.id, tweet_page)
+
+        # Get list of tweet_ids saved in the DB
         saved_ids = map(lambda t: t.id,
                         Tweet.query.filter(Tweet.id.in_(tweet_ids)))
+
+        # Filter out any tweets that have already been saved
         unsaved = filter(lambda t: t.id not in saved_ids, tweet_page)
-        if len(unsaved) > 0:
+
+        # Save any unsaved tweets
+        if unsaved:
             page += 1
-            map(save_tweet, unsaved)
+            for tweet in unsaved:
+                save_tweet(tweet)
             logger.info("Saved {0} tweets.".format(len(unsaved)))
         else:
-            return
+            break  # Break if there are no unsaved tweets in this page
 
 
 @scheduler.scheduled_job('interval', hours=12)
