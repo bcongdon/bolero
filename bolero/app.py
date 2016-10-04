ba9da7495ab73f93a255f4d3d2c71c7f81e7be5a
Benjamin Congdon <bencongdon96@gmail.com>
2016-10-04 06:49:00 -0500

    Added database creation step to app initialization

--- bolero/app.py
+++ bolero/app.py
@@ -5,6 +5,8 @@ import logging
 from .utils import get_loaded_trackers, get_config_file
 from . import tracker_classes
 from .scheduler import scheduler
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
 import os
 
 logger = logging.getLogger(__name__)
@@ -12,8 +14,8 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['AUTH_KEYS'] = dict()
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ['SQL_URL'] or
-                                         get_config_file()['sql_url'])
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('SQL_URI') or
+                                         get_config_file()['sql_uri'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Setup logging
@@ -30,7 +32,14 @@ def load_trackers():
             x.service_name in loaded_trackers]
 
 
+def create_db():
+    engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+
 def setup():
+    create_db()
     db.init_app(app)
 
     for t in load_trackers():
