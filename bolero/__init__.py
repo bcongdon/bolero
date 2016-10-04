96fd2ad1198ee974a49402213270068076016da3
Benjamin Congdon <bencongdon96@gmail.com>
2016-10-04 06:15:04 -0500

    python3 compatible module imports, downgrading test suite to python2

--- bolero/__init__.py
+++ bolero/__init__.py
@@ -1,7 +1,7 @@
-from trackers.twitter import TwitterTracker
-from trackers.myfitnesspal_tracker import MyFitnessPalTracker
-from trackers.withings_tracker import WithingsTracker
-from trackers.wunderlist import WunderlistTracker
+from .trackers.twitter import TwitterTracker
+from .trackers.myfitnesspal_tracker import MyFitnessPalTracker
+from .trackers.withings_tracker import WithingsTracker
+from .trackers.wunderlist import WunderlistTracker
 
 tracker_classes = [
     TwitterTracker,
