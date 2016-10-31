61bb746fb2b185aefdd7d3b754ff68edec0d5658
Benjamin Congdon <bencongdon96@gmail.com>
2016-10-31 11:34:34 -0500

    work towards todoist tracker

---
+++ bolero/trackers/todoist_tracker.py
@@ -0,0 +1,21 @@
+import todoist
+from . import db
+import logging
+from .tracker import BoleroTracker
+from ..utils import requires
+logger = logging.getLogger(__name__)
+
+class Task(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True))
+    completed_at = db.Column(db.DateTime(timezone=True))
+    list_id = db.Column(db.BigInteger, db.ForeignKey('list.id'))
+
+class TodoistTracker(BoleroTracker):
+
+    @requires('todoist.username', 'todoist.password')
+    def handle_authentication(self, config):
+        api = todoist.TodoistAPI()
+        return api.user.login(config['todoist.username'],
+                              config['todoist.password'])
