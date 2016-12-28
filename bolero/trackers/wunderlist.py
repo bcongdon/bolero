6ebe70408d3cd3d091eeaeecab26e2c13a8cd77f
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-28 14:27:22 -0800

    Rename Wunderlist tables

--- bolero/trackers/wunderlist.py
+++ bolero/trackers/wunderlist.py
@@ -8,17 +8,18 @@ from itertools import chain
 logger = logging.getLogger(__name__)
 
 
-class Task(db.Model):
+class WunderlistTask(db.Model):
     id = db.Column(db.BigInteger, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True))
     completed_at = db.Column(db.DateTime(timezone=True))
-    list_id = db.Column(db.BigInteger, db.ForeignKey('list.id'))
+    list_id = db.Column(db.BigInteger, db.ForeignKey('wunderlist_list.id'))
 
     @staticmethod
     def save_or_update(t):
-        task = (Task.query.filter(Task.id == t['id']).first() or
-                Task(id=t['id']))
+        task = (WunderlistTask.query.filter(
+                    WunderlistTask.id == t['id']).first() or
+                WunderlistTask(id=t['id']))
         task.title = t['title']
         task.created_at = parse(t['created_at'])
         task.list_id = t['list_id']
@@ -28,18 +29,19 @@ class Task(db.Model):
         db.session.commit()
 
 
-class List(db.Model):
+class WunderlistList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True))
     public = db.Column(db.Boolean)
-    tasks = db.relationship('Task', backref='list',
+    tasks = db.relationship('WunderlistTask', backref='list',
                             lazy='dynamic')
 
     @staticmethod
     def save_or_update(l):
-        _list = (List.query.filter(List.id == l['id']).first() or
-                 List(id=l['id']))
+        _list = (WunderlistList.query.filter(
+                    WunderlistList.id == l['id']).first() or
+                 WunderlistList(id=l['id']))
         _list.title = l['title']
         _list.created_at = l['created_at']
         _list.public = l['public']
@@ -67,13 +69,13 @@ class WunderlistTracker(BoleroTracker):
         """
         api = self.client
         lists = api.get_lists()
-        map(List.save_or_update, lists)
+        map(WunderlistList.save_or_update, lists)
         list_ids = map(lambda x: x['id'], lists)
         uncompleted = (api.get_tasks(_id) for _id in list_ids)
         completed = (api.get_tasks(_id, completed=True) for _id in list_ids)
         total = (i for sublist in chain(uncompleted, completed)
                  for i in sublist)
-        map(Task.save_or_update, total)
+        map(WunderlistTask.save_or_update, total)
 
     def create_api(self, manager):
-        manager.create_api(Task, methods=['GET'])
+        manager.create_api(WunderlistTask, methods=['GET'])
