4813186472fa3062f40ed2a833e2e45f8705c4b4
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-29 14:58:57 -0800

    Update to make fitbit tracker use more efficient API calls

--- bolero/trackers/fitbit_tracker.py
+++ bolero/trackers/fitbit_tracker.py
@@ -5,6 +5,7 @@ from . import db
 from .tracker import BoleroTracker
 import datetime
 from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
 import logging
 logger = logging.getLogger(__name__)
 
@@ -30,47 +31,71 @@ class FitbitTracker(BoleroTracker):
                              access_token=config['fitbit.access_token'],
                              refresh_token=config['fitbit.refresh_token'])
 
-    def save_or_update(self, date=datetime.date.today()):
-        day = self.client.activities(date=date)['summary']
+    def save_or_update(self, date, steps, caloriesOut, floors,
+                       lightly_active_minutes, fairly_active_minutes,
+                       very_active_minutes, distance):
+
         db_date = (FitbitDay.query.filter_by(date=date).first() or
                    FitbitDay(date=date))
-        db_date.steps = day['steps']
-        db_date.calories_burned = day['caloriesOut']
-        db_date.floors = day['floors']
-        db_date.lightly_active_minutes = day['lightlyActiveMinutes']
-        db_date.fairly_active_minutes = day['fairlyActiveMinutes']
-        db_date.very_active_minutes = day['veryActiveMinutes']
-        db_date.miles = next(i for i in day['distances']
-                             if i['activity'] == 'total')['distance']
+        db_date.date = date
+        db_date.steps = steps
+        db_date.calories_burned = caloriesOut
+        db_date.floors = floors
+        db_date.lightly_active_minutes = lightly_active_minutes
+        db_date.fairly_active_minutes = fairly_active_minutes
+        db_date.very_active_minutes = very_active_minutes
+        db_date.miles = distance
         db.session.add(db_date)
         db.session.commit()
 
+    def fetch_day(self, date):
+        day = self.client.activities(date=date)['summary']
+        self.save_or_update(
+            date,
+            day['steps'],
+            day['caloriesOut'],
+            day['floors'],
+            day['lightlyActiveMinutes'],
+            day['fairlyActiveMinutes'],
+            day['veryActiveMinutes'],
+            next(i for i in day['distances']
+                 if i['activity'] == 'total')['distance'])
+
     def save_range(self, start, end):
-        date = start
-        while date <= datetime.date.today():
-            try:
-                self.save_or_update(date)
-            except fitbit.exceptions.HTTPTooManyRequests:
-                next_hour = datetime.datetime.now()
-                next_hour = (next_hour.replace(minute=0, second=30) +
-                             datetime.timedelta(hours=1))
-                logger.info("Got rate limited. Resuming at " +
-                            next_hour.isoformat())
-                scheduler.add_job(self.save_range, args=(date, end),
-                                  next_run_time=next_hour)
-                break
-            date += datetime.timedelta(days=1)
+        attributes = ['steps', 'calories', 'floors', 'minutesLightlyActive',
+                      'minutesFairlyActive', 'minutesVeryActive', 'distance']
+        while start <= end:
+            data = {}
+            for a in attributes:
+                res = self.client.time_series(
+                    'activities/' + a,
+                    period='1y',
+                    base_date=start)
+                data[a] = {i['dateTime']: i['value']
+                           for i in res[res.keys()[0]]}
+            dates = data['steps'].keys()
+            for d in dates:
+                self.save_or_update(
+                    parse(d).date(),
+                    data['steps'][d],
+                    data['calories'][d],
+                    data['floors'][d],
+                    data['minutesLightlyActive'][d],
+                    data['minutesFairlyActive'][d],
+                    data['minutesVeryActive'][d],
+                    data['distance'][d]
+                )
+            start += relativedelta(years=1)
 
     def update(self):
         date = datetime.date.today()
         for i in range(7):
             self.save_or_update(date=date - datetime.timedelta(days=i))
 
-    @requires('fitbit.start_date')
-    def backfill(self, config):
-        start = parse(config['fitbit.start_date']).date()
+    def backfill(self):
+        profile = self.client.user_profile_get()
+        start = parse(profile['user']['memberSince']).date()
         self.save_range(start, datetime.date.today())
-        
 
     def create_api(self, manager):
         manager.create_api(FitbitDay)
