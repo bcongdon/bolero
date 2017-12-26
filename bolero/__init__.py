e2b3bd3ac0fbb3e74238b73bd665d33da41d8e67
Benjamin Congdon <bencongdon96@gmail.com>
2017-12-26 08:02:59 -0800

    Consistent tracker naming

--- bolero/__init__.py
+++ bolero/__init__.py
@@ -1,9 +1,9 @@
 from .trackers.fitbit_tracker import FitbitTracker
 from .trackers.myfitnesspal_tracker import MyFitnessPalTracker
-from .trackers.nokia_health import NokiaHealthTracker
+from .trackers.nokia_health_tracker import NokiaHealthTracker
 from .trackers.todoist_tracker import TodoistTracker
-from .trackers.twitter import TwitterTracker
-from .trackers.wunderlist import WunderlistTracker
+from .trackers.twitter_tracker import TwitterTracker
+from .trackers.wunderlist_tracker import WunderlistTracker
 
 tracker_classes = [
     FitbitTracker,
